bf34f0bef49eabcde4f8294c83429a4f6199ca8e
Son Nguyen <lamson1207@gmail.com>
2026-04-24 23:01:06 +0300

    Complete Phase 1 controller with @FXML fields

--- src/main/java/fi/oop/MainController.java
+++ src/main/java/fi/oop/MainController.java
@@ -1,5 +1,38 @@
 package fi.oop;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
 public class MainController {
-    
+    @FXML
+    private ListView<String> itemListView;
+
+    @FXML
+    private Label itemInfoLabel;
+
+    @FXML
+    private ComboBox<String> userComboBox;
+
+    @FXML
+    private Label userInfoLabel;
+
+    @FXML
+    private Label statusLabel;
+
+    @FXML
+    private void handleUserSelection() {
+        statusLabel.setText("User selected");
+    }
+
+    @FXML
+    private void handleBorrow() {
+        statusLabel.setText("Borrow clicked");
+    }
+
+    @FXML
+    private void handleReturn() {
+        statusLabel.setText("Return clicked");
+    }
 }
